ad3027b10a13dde7a5177404a4bda0b630899d01
Karsten Dambekalns <karsten@typo3.org>
2009-10-22 16:47:25 +0000

    [~TASK] ExtJS: Added @api annotations on class level. [~TASK] PHP6: Added @api annotations on class level. [~TASK] Testing: Added @api annotations on class level. [~TASK] YAML: Added @api annotations on class level. [~TASK] Party: Removed leftover @package/@subpackage annotations.

--- Classes/ViewHelpers/IncludeViewHelper.php
+++ Classes/ViewHelpers/IncludeViewHelper.php
@@ -28,6 +28,7 @@ namespace F3\ExtJS\ViewHelpers;
  * @version $Id$
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  * @scope prototype
+ * @api
  */
 class IncludeViewHelper extends \F3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
